957c126d1258a16448af4be5c220c3b03eb4ac76
Niccolò Maltoni <niccolo@maltoni.dev>
2024-10-13 21:44:34 +0200

    feat: add simple client-server setup

--- src/main/java/com/diennea/carapace/Main.java
+++ src/main/java/com/diennea/carapace/Main.java
@@ -1,18 +1,37 @@
 package com.diennea.carapace;
 
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.Provider;
-import java.security.Security;
-import java.util.Arrays;
+import io.netty.handler.logging.LogLevel;
+import java.net.URI;
+import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufFlux;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.tcp.SslProvider;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 public class Main {
 
-    public static void main(final String... args) throws KeyStoreException {
-        final Provider[] providers = Security.getProviders();
-        System.out.println("Providers: " + Arrays.toString(providers));
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
 
-        final KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-        System.out.println("KeyStore: " + keyStore.getType());
+    public static void main(final String... args) {
+        final HttpServer httpServer = HttpServer
+                .create()
+                .host(HOST)
+                .port(PORT)
+                .wiretap(HttpServer.class.getName(), LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL)
+                .handle((request, response) -> response.sendString(Mono.just("Hello from server")));
+        httpServer.warmup().block();
+        final DisposableServer server = httpServer.bindNow();
+
+        final HttpClient client = HttpClient
+                .create()
+                .host(HOST)
+                .port(PORT)
+                .wiretap(HttpClient.class.getName(), LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
+        client.warmup().block();
+        client.get().response().block();
+        server.onDispose().block();
     }
 }
