2aa1fec1ada995ec7f8665bc5cfe77fb52274f3a
Niccolò Maltoni <niccolo@maltoni.dev>
2024-10-13 19:04:02 +0200

    feat: add sample prints

--- src/main/java/com/diennea/carapace/Main.java
+++ src/main/java/com/diennea/carapace/Main.java
@@ -1,5 +1,18 @@
 package com.diennea.carapace;
 
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Arrays;
+
 public class Main {
 
+    public static void main(final String... args) throws KeyStoreException {
+        final Provider[] providers = Security.getProviders();
+        System.out.println("Providers: " + Arrays.toString(providers));
+
+        final KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        System.out.println("KeyStore: " + keyStore.getType());
+    }
 }
